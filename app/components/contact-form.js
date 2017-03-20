9c665c3a4e4339d3d2d69de582ecab9cfb5484e5
Kenny Tsui <kjtsui15@gmail.com>
2017-03-20 08:40:08 -0400

    converting contact us page to react

---
+++ app/components/contact-form.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export default class ContactForm extends React.Component {
+    render() {
+        return (
+            <form role="form" data-toggle="validator">
+                <div className="container col-md-8 col-md-offset-2 contact-form">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label htmlFor="inputName" className="control-label">Full Name:</label>
+                                <input type="text" className="form-control" id="inputName" required data-error="Failure"></input>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label htmlFor="inputEmail" className="control-label">Email:</label>
+                                <input type="email" className="form-control" id="inputEmail" required data-error="Failure">
+                                    <div className="help-block with-errors"></div>
+                                </input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <label htmlFor="usr">Category:</label>
+                                <select className="form-control" id="sel1">
+                                    <option selected>General</option>
+                                    <option>Legal</option>
+                                    <option>Partnership</option>
+                                    <option>Payment</option>
+                                    <option>Other</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <label htmlFor="usr">Message:</label>
+                                <textarea className="form-control" rows="5" id="message" required data-error=""></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <button type="submit" className="form-control btn btn-default contact-submit">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        )
+    }
+}
---
+++ app/components/contact-us.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import ContactForm from './contact-form';
+import ContactInformation from './contact-information';
+
+export default class ContactUs extends React.Component {
+    render() {
+        return (
+            <div className="container contact-container">
+                <div className="row">
+                    <div className="col-md-8 col-md-offset-2 contact-title">
+                        <h1>
+                            <u>Contact Us</u>
+                        </h1>
+                    </div>
+                    <ContactForm />
+                </div>
+                <div className="row">
+                    <div className="col-md-8 col-md-offset-2 contact-title">
+                        <h1>
+                            <u>Company Information</u>
+                        </h1>
+                        <ContactInformation />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}

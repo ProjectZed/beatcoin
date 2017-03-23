226ecc6c91ffabaf3cbf181d44060ac2a076dd07
Yueying Liu <648339108@qq.com>
2017-03-23 17:49:29 -0400

    Solved non-scrolling issue

--- app/components/user-home.js
+++ app/components/user-home.js
@@ -8,20 +8,20 @@ export default class UserHome extends React.Component {
     return (
       // main thing -->
       <div className="container panelstyle">
-        <div className = "row"> //overall row -->
+        <div className = "row">
 
-              //left empty column -->
+
               <div className = "col-md-1">
               </div>
 
-              <div className = "col-md-10"> // overall column -->
+              <div className = "col-md-10">
 
-                //The row for User Preferred Playlist -->
+
                 <PreferredPlaylist pic = "img/user-home/Playlist.jpg"></PreferredPlaylist>
 
-                 // end of row for User Preferred Playlist -->
 
-                // Row for the first line of categories -->
+
+
                 <div className = "row everything">
 
                 <Playlist pic = "img/user-home/Jazz.jpg" listname = "Jazz"></Playlist>
@@ -30,43 +30,43 @@ export default class UserHome extends React.Component {
                 <Playlist pic = "img/user-home/Blues.jpg" listname = "Blues"></Playlist>
 
 
-                </div> // End of the first line of categories -->
+                </div>
 
-                // Row for the second line of categories -->
+
                 <div className = "row everything">
                   <Playlist pic = "img/user-home/Funk.jpg" listname = "Funk"></Playlist>
                   <Playlist pic = "img/user-home/Country.jpg" listname = "Country"></Playlist>
                   <Playlist pic = "img/user-home/Pop.jpg" listname = "Pop"></Playlist>
                   <Playlist pic = "img/user-home/RnB.jpg" listname = "RnB"></Playlist>
 
-                </div> // End of the second line of categories -->
+                </div>
 
-                // Row for the third line of categories -->
+
                 <div className = "row everything">
                   <Playlist pic = "img/user-home/Hiphop.jpg" listname = "Hip Hop"></Playlist>
                   <Playlist pic = "img/user-home/Rapping.jpg" listname = "Rapping"></Playlist>
                   <Playlist pic = "img/user-home/Reggae.jpg" listname = "Reggae"></Playlist>
                   <Playlist pic = "img/user-home/Punk.jpg" listname = "Punk"></Playlist>
 
-                </div> // End of the third line of categories -->
+                </div>
 
-                // Row for the fourth line of categories -->
+
                 <div className = "row">
-                  <Playlist pic = "img/user-home/Instrumental.jpg" listname = "Instrumental"></Playlist>
+                  <Playlist pic = "img/user-home/Instrumental.png" listname = "Instrumental"></Playlist>
                   <Playlist pic = "img/user-home/EDM.jpg" listname = "EDM"></Playlist>
-                  <Playlist pic = "img/user-home/classNameical.jpg" listname = "classNameical"></Playlist>
+                  <Playlist pic = "img/user-home/Classical.jpg" listname = "Classical"></Playlist>
                   <Playlist pic = "img/user-home/Acapella.jpg" listname = "Acapella"></Playlist>
 
-                </div> // End of the fourth line of categories -->
+                </div>
 
-              </div> // end of overall column -->
+              </div>
 
-              // right empty column -->
+
               <div className = "col-md-1">
               </div>
 
-        </div> // end of overall row -->
-      </div> // end of container -->
+        </div>
+      </div> 
 
     );
   }

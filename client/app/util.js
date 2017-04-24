fde60c89f68127970474825574229d1ffb0ad792
Yueying Liu <648339108@qq.com>
2017-04-24 18:12:57 -0400

    ErrorBanner made

--- client/app/util.js
+++ client/app/util.js
@@ -9,3 +9,14 @@ let options = {
 export function unixTimeToString(time) {
   return new Date(time).toLocaleString('en-us', options).replace(",", "");
 }
+
+/**
+ * If shouldHide is true, returns a CSS class that hides the element.
+ */
+export function hideElement(shouldHide) {
+  if (shouldHide) {
+    return 'hidden';
+  } else {
+    return '';
+  }
+}

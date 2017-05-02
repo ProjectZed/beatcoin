85d20cadc3eb1cb259c0faceae6f6d49aec8cf08
Yueying Liu <648339108@qq.com>
2017-05-02 18:28:13 -0400

    Added beatcoin logo before the beatcoin number

--- client/app/components/navbar.js
+++ client/app/components/navbar.js
@@ -47,9 +47,10 @@ export default class Navbar extends React.Component {
               <div className="btn-toolbar pull-right" role="toolbar">
                 <div className="btn-group" role="group">
                   <button type="button" className="btn btn-default navbar-btn">
+                    <img src="img/beatcoinholder.png" id="beatcoin-small"/>
                     <Link to="/redeem" style={{
                       'color': 'white'
-                    }}>{beatcoins}</Link>
+                    }}>  {beatcoins}</Link>
                   </button>
                 </div>
                 <div className="btn-group" role="group">
